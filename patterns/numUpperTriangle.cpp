8618f5a9665078a4b3e9f8681ecc49f66e2e276b
Aditya Rawat <rawataditya795@gmail.com>
2023-06-05 00:32:33 +0530

    added new file

---
+++ patterns/numUpperTriangle.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    
+    for(int i = 1;i <= 5;i++){
+        //here we just have to print j
+        //because no of columns = rows 
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j;
+        }
+
+        cout<<""<<endl;
+    }
+}
